91b745fa00b8c08d86b7252620b609e9c7203565
swendo-ace <s.wendo@alustudent.com>
2026-07-05 04:45:52 -0700

    Add 12-roman_to_int.py

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str:
+        return 0
+    values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    total = 0
+    for i in range(len(roman_string)):
+        current = values.get(roman_string[i], 0)
+        if i + 1 < len(roman_string):
+            next_value = values.get(roman_string[i + 1], 0)
+            if current < next_value:
+                total -= current
+            else:
+                total += current
+        else:
+            total += current
+    return total
