c52ea66d6aa7421d2aa27e55fff23a84947e47db
swendo-ace <s.wendo@alustudent.com>
2026-07-31 13:20:46 -0700

    Add task 7: load, add args, save to JSON

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Adds command-line arguments to a list saved in a JSON file."""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
