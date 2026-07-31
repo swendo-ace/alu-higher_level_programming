b3bc309452f4da7a2fe78e299a7df41d20ed72e4
swendo-ace <s.wendo@alustudent.com>
2026-07-31 13:08:29 -0700

    Fix PEP8 line length in 2-append_write

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Append a string to the end of a UTF-8 text file, returning chars added."""
+    """Append a string to a UTF-8 file and return chars added."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
