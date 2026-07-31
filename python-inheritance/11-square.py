0ffe675760cebe3d2fd9381368fffeae660e01f7
swendo-ace <s.wendo@alustudent.com>
2026-07-31 12:59:27 -0700

    Add task 11: Square with own str representation

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a Square class that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: the size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the square description: [Square] <width>/<height>."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
