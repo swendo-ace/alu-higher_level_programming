9ff9cdc0e430f365af2cfb4e526ce80cc4567880
swendo-ace <s.wendo@alustudent.com>
2026-07-31 12:44:00 -0700

    Add task 6: BaseGeometry with abstract area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Represents base geometry."""
+
+    def area(self):
+        """Raise an Exception; area is not implemented here."""
+        raise Exception("area() is not implemented")
