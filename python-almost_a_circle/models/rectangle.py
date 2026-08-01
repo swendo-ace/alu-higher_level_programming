24459b5f66b0d9613a3516c9fa7608024191a68c
swendo-ace <s.wendo@alustudent.com>
2026-08-01 12:50:03 -0700

    Add task 5: display method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,8 @@ class Rectangle(Base):
     def area(self):
         """Return the area of the rectangle."""
         return self.__width * self.__height
+
+    def display(self):
+        """Print the rectangle using the # character."""
+        for i in range(self.__height):
+            print("#" * self.__width)
