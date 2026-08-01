dbadfb7db62f17a5b18beb6270809bf6a772acf7
swendo-ace <s.wendo@alustudent.com>
2026-08-01 13:19:51 -0700

    Add task 12: Square update method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,6 +28,21 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """Update attributes via args (ordered) or kwargs (named).
+
+        Args:
+            *args: id, size, x, y (in order)
+            **kwargs: attribute names and values (skipped if args given)
+        """
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for attr, value in zip(attrs, args):
+                setattr(self, attr, value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         """Return the square description."""
         return "[Square] ({}) {}/{} - {}".format(
