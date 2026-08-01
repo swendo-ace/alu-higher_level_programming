b65bb8ca9ed97d31f40619094b0b1acb696a4c72
swendo-ace <s.wendo@alustudent.com>
2026-08-01 13:42:02 -0700

    Add task 18: create class method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -43,3 +43,15 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes set from a dictionary."""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = cls()
+        dummy.update(**dictionary)
+        return dummy
