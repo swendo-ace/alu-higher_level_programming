3583d69a0bb6e385cf0d847ee4082c2043c3112c
swendo-ace <s.wendo@alustudent.com>
2026-08-01 12:52:28 -0700

    Add task 6: __str__ method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,8 @@ class Rectangle(Base):
         """Print the rectangle using the # character."""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Return the rectangle description."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
