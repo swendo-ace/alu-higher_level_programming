4921cd8874aac8093421dbf3da8a11f5b4cef912
swendo-ace <s.wendo@alustudent.com>
2026-08-01 03:40:39 -0700

    Add task 1: display body of 200 response

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Takes a URL, sends a request, displays the response body size in bytes
 curl -s "$1" | wc -c
---
+++ python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends a GET request to a URL, displays the body only for a 200 response
+curl -s -f "$1"
