8dc27c676556f76d49036b8e68238afaf86ae67e
swendo-ace <s.wendo@alustudent.com>
2026-08-01 13:12:27 -0700

    Add Square class and tests

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines the Square class, inheriting from Rectangle."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square, inheriting from Rectangle."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square.
+
+        Args:
+            size: the size (used as both width and height)
+            x: the x position
+            y: the y position
+            id: the identity (passed to Base)
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return the square description."""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
