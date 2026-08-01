befe1314eedd88316e9a3c7bc10e292acc6bbd2c
swendo-ace <s.wendo@alustudent.com>
2026-08-01 06:18:20 -0700

    Trim function docstring to 3 lines

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,10 +7,7 @@ are int or float, casts them to integers, and returns their sum.
 
 
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
-
-    a and b must be integers or floats, otherwise a TypeError is raised.
-    """
+    """Return the integer addition of a and b, casting floats to int."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
