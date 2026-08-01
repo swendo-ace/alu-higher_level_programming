77686d2f4859faca812a1e7c8d702a6e70a8d5e2
swendo-ace <s.wendo@alustudent.com>
2026-08-01 04:56:28 -0700

    Add task 6: POST an email with requests

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Sends a POST request with an email using the requests library."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data={"email": email})
+    print(r.text)
